367b2f0fe0fe5cea82f51f231a8b8e0618a32389
Owen <owenvandermeer@gmail.com>
2022-06-17 09:17:21 -0400

    All options now appear in form

--- polls/models.py
+++ polls/models.py
@@ -51,11 +51,66 @@ Q2_CHOICES = (
     ('4', 'None'),
 )
 
+Q3_CHOICES = (
+    ('1', 'Regimented prayer schedule'),
+    ('2', 'Recommended prayer'),
+    ('3', 'Meditation'),
+    ('4', 'Group prayer'),
+)
+
+Q4_CHOICES = (
+    ('1', 'Eating And Drinking Restrictions'),
+    ('2', 'Fasting'),
+    ('3', 'Who you can marry'),
+    ('4', 'Pre-marital sex'),
+    ('5', 'Virtually None'),
+    ('6', 'Cutting hair'),
+    ('7', 'Gender Identity/Sexuality'),
+)
+
+Q5_CHOICES = (
+    ('1', 'Yes'),
+    ('2', 'No'),
+)
+
+Q6_CHOICES = (
+    ('1', 'Worships in a building of religious significance of a specific week day each week'),
+    ('2', 'Often happens in a building of religious significance but sometimes happens in an informal setting, on a specific day of the week'),
+    ('3', 'Worship is not on a specific day, or no common gathering place'),
+)
+
+Q7_CHOICES = (
+    ('1', 'Yes'),
+    ('2', 'No'),
+)
+
+Q8_CHOICES = (
+    ('1', 'Yes'),
+    ('2', 'No'),
+)
+
+Q9_CHOICES = (
+    ('1', 'Start showing up'),
+    ('2', 'Specific ritual'),
+    ('3', 'Through birth or a special council'),
+)
+
+Q10_CHOICES = (
+    ('1', 'No specific way'),
+    ('2', 'The heavens and earth were created in six days, and on the seventh day, God rested.'),
+    ('3', 'The God(s) created it and it is just an extension of the God(s)')
+)
+
+Q11_CHOICES = (
+    ('1', 'Through the clergy'),
+    ('2', 'Personal Relationship'),
+)
+
 class Response(models.Model):
     name = models.CharField(max_length=100)
     q1 = models.CharField(choices=Q1_CHOICES, max_length=200, verbose_name="What do you believe happens after you die?")
     q2 = models.CharField(choices=Q2_CHOICES, max_length=200, verbose_name="How many deities are in your religion?")
-    q3 = models.CharField(max_length=10)
+    q3 = models.CharField(max_length=10, choices=Q3_CHOICES, verbose_name="Types And Amount Of Prayer?")
     q4 = models.CharField(max_length=10)
     q5 = models.CharField(max_length=10)
     q6 = models.CharField(max_length=10)
@@ -71,7 +126,16 @@ class Response(models.Model):
 
 class ResponseForm(ModelForm):
     q1 = forms.ChoiceField(choices=Q1_CHOICES, widget=forms.RadioSelect)
-    q2 = forms.MultipleChoiceField(choices=Q2_CHOICES, widget=forms.CheckboxSelectMultiple)
+    q2 = forms.ChoiceField(choices=Q2_CHOICES, widget=forms.RadioSelect)
+    q3 = forms.ChoiceField(choices=Q3_CHOICES, widget=forms.RadioSelect)
+    q4 = forms.MultipleChoiceField(choices=Q4_CHOICES, widget=forms.CheckboxSelectMultiple)
+    q5 = forms.ChoiceField(choices=Q5_CHOICES, widget=forms.RadioSelect)
+    q6 = forms.ChoiceField(choices=Q6_CHOICES, widget=forms.RadioSelect)
+    q7 = forms.ChoiceField(choices=Q7_CHOICES, widget=forms.RadioSelect)
+    q8 = forms.ChoiceField(choices=Q8_CHOICES, widget=forms.RadioSelect)
+    q9 = forms.ChoiceField(choices=Q9_CHOICES, widget=forms.RadioSelect)
+    q10 = forms.ChoiceField(choices=Q10_CHOICES, widget=forms.RadioSelect)
+    q11 = forms.ChoiceField(choices=Q11_CHOICES, widget=forms.RadioSelect)
 
     class Meta:
         model = Response
