6fe6e4e617671bf03ec27168c48b377708c76c82
Owen V <owenvandermeer@gmail.com>
2022-06-27 00:04:09 -0400

    fixed settings.py for while running collectstatic

--- RS_Django_Project/settings.py
+++ RS_Django_Project/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'RS_Django_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'RS_Django_Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
